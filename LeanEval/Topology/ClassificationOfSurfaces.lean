e6c1c7dd9ca291c0533a869567607f9ba93c0acb
Junyan Xu <junyanxu.math@gmail.com>
2026-05-02 12:44:19 +0200

    remove notation causing CI failure

--- LeanEval/Topology/ClassificationOfSurfaces.lean
+++ LeanEval/Topology/ClassificationOfSurfaces.lean
@@ -53,12 +53,11 @@ inductive NonOrientableRel (p n : ℕ) : ClosedUnitDisc → ClosedUnitDisc → P
       (.bdyPtOfReal <| -(3 * i + x) / (2 * p + 3 * n))
       (.bdyPtOfReal <| -(3 * i + 3 - x) / (2 * p + 3 * n))
 
-open scoped Manifold
-
 @[eval_problem]
 theorem classification_of_surfaces (S : Type*) [TopologicalSpace S]
     [T2Space S] [ConnectedSpace S] [CompactSpace S]
-    [ChartedSpace (EuclideanHalfSpace 2) S] [IsManifold (𝓡∂ 2) 0 S] :
+    [ChartedSpace (EuclideanHalfSpace 2) S]
+    [IsManifold (modelWithCornersEuclideanHalfSpace 2) 0 S] :
     Nonempty (S ≃ₜ Metric.sphere (0 : EuclideanSpace ℝ (Fin 3)) 1) ∨
     ∃ p n, ((1 ≤ p ∨ 1 ≤ n) ∧ Nonempty (S ≃ₜ Quot (OrientableRel p n))) ∨
       (1 ≤ p ∧ Nonempty (S ≃ₜ Quot (NonOrientableRel p n))) := by
