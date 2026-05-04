bf0ef7fe5d923ae5ca5f3f252028ea5a7d9300c8
Junyan Xu <junyanxu.math@gmail.com>
2026-05-05 00:54:18 +0200

    swap σ with σ.symm (review by @adamtopaz)

--- LeanEval/NumberTheory/NeukirchUchida.lean
+++ LeanEval/NumberTheory/NeukirchUchida.lean
@@ -19,8 +19,8 @@ namespace LeanEval.NumberTheory
 theorem neukirch_uchida {K₁ K₂ K₁' K₂' : Type*} [Field K₁] [Field K₂] [Field K₁'] [Field K₂']
     [NumberField K₁] [NumberField K₂] [Algebra K₁ K₁'] [Algebra K₂ K₂'] [IsSepClosure K₁ K₁']
     [IsSepClosure K₂ K₂'] (ϕ : Gal(K₁'/K₁) ≃* Gal(K₂'/K₂)) (he : IsHomeomorph ϕ) :
-    ∃! σ : K₁' ≃+* K₂', (algebraMap K₁ K₁').range.map σ.toRingHom = (algebraMap K₂ K₂').range ∧
-      ∀ g : Gal(K₁'/K₁), ϕ g = σ.symm.trans (g.toRingEquiv.trans σ) := by
+    ∃! σ : K₂' ≃+* K₁', (algebraMap K₂ K₂').range.map σ.toRingHom = (algebraMap K₁ K₁').range ∧
+      ∀ g : Gal(K₁'/K₁), ϕ g = σ.trans (g.toRingEquiv.trans σ.symm) := by
   sorry
 
 end LeanEval.NumberTheory
