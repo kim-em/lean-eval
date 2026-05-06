f53261d16086d7ded397159799c79b5ea21953e9
Julia Markus Himmel <2065352+TwoFX@users.noreply.github.com>
2026-05-06 10:25:17 +0200

    style

--- LeanEval/Combinatorics/BalanceableBoundedPartitions.lean
+++ LeanEval/Combinatorics/BalanceableBoundedPartitions.lean
@@ -11,7 +11,7 @@ A partition is `k`-bounded if all of its members are at most `k`.
 For example, `12 = 3 + 2 + 2 + 2 + 2 + 1` is a `3`-bounded partition
 of `12`.
 -/
-def Bounded {n : Nat} (k : Nat) (p : n.Partition) : Prop :=
+def Bounded {n : ℕ} (k : ℕ) (p : n.Partition) : Prop :=
   ∀ i ∈ p.parts, i ≤ k
 
 /--
@@ -21,7 +21,7 @@ the same size.
 For example, `12 = (3 + 2 + 1) + (2 + 2 + 2)` is a balanceable partition
 of `12`.
 -/
-def Balanceable {n : Nat} (p : n.Partition) : Prop :=
+def Balanceable {n : ℕ} (p : n.Partition) : Prop :=
   ∃ s₁ s₂, s₁ + s₂ = p.parts ∧ s₁.sum = s₂.sum
 
 /--
@@ -29,7 +29,7 @@ For any `k`, the smallest `n` such that any `k`-bounded partition of `n` is
 also balanceable is given by `2 * lcm(1, ..., k)`.
 -/
 @[eval_problem]
-theorem minimal_boundedImpliesBalanceable (k : Nat) (hk : 0 < k) :
+theorem minimal_balanceable_of_bounded (k : ℕ) (hk : 0 < k) :
     Minimal (fun n => 0 < n ∧ ∀ p : n.Partition, Bounded k p → Balanceable p) (2 * (Finset.Icc 1 k).lcm id) := by
   sorry
 
